1bffbec31054548a6d0e775d25d3f55cac3e9b1d
miguel rodriguez alvarez <miguel7.254@gmail.com>
2024-07-09 12:46:05 +0200

    añadido añadir boton

--- src/Listify.UI/MainForm.Designer.cs
+++ src/Listify.UI/MainForm.Designer.cs
@@ -29,30 +29,77 @@
         private void InitializeComponent()
         {
             combinationslvw = new ListView();
+            CombinationsLbl = new Label();
+            AddWinningtxtbox = new TextBox();
+            AddWinningLbl = new Label();
+            AddWinningBtn = new Button();
             SuspendLayout();
             // 
             // combinationslvw
             // 
-            combinationslvw.Location = new Point(12, 89);
+            combinationslvw.Location = new Point(12, 34);
             combinationslvw.Name = "combinationslvw";
             combinationslvw.Size = new Size(255, 317);
             combinationslvw.TabIndex = 0;
             combinationslvw.UseCompatibleStateImageBehavior = false;
+            // 
+            // CombinationsLbl
+            // 
+            CombinationsLbl.AutoSize = true;
+            CombinationsLbl.Location = new Point(12, 16);
+            CombinationsLbl.Name = "CombinationsLbl";
+            CombinationsLbl.Size = new Size(148, 15);
+            CombinationsLbl.TabIndex = 1;
+            CombinationsLbl.Text = "Combinaciones premiadas";
+            // 
+            // AddWinningtxtbox
+            // 
+            AddWinningtxtbox.Location = new Point(306, 34);
+            AddWinningtxtbox.Name = "AddWinningtxtbox";
+            AddWinningtxtbox.Size = new Size(191, 23);
+            AddWinningtxtbox.TabIndex = 2;
+            // 
+            // AddWinningLbl
+            // 
+            AddWinningLbl.AutoSize = true;
+            AddWinningLbl.Location = new Point(304, 16);
+            AddWinningLbl.Name = "AddWinningLbl";
+            AddWinningLbl.Size = new Size(193, 15);
+            AddWinningLbl.TabIndex = 3;
+            AddWinningLbl.Text = "Añadir a combinaciones premiadas";
+            // 
+            // AddWinningBtn
+            // 
+            AddWinningBtn.Location = new Point(503, 16);
+            AddWinningBtn.Name = "AddWinningBtn";
+            AddWinningBtn.Size = new Size(79, 41);
+            AddWinningBtn.TabIndex = 4;
+            AddWinningBtn.Text = "Añadir";
+            AddWinningBtn.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(AddWinningBtn);
+            Controls.Add(AddWinningLbl);
+            Controls.Add(AddWinningtxtbox);
+            Controls.Add(CombinationsLbl);
             Controls.Add(combinationslvw);
             Name = "MainForm";
             Text = "Listify App";
             Load += MainForm_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private ListView combinationslvw;
+        private Label CombinationsLbl;
+        private TextBox AddWinningtxtbox;
+        private Label AddWinningLbl;
+        private Button AddWinningBtn;
     }
 }
